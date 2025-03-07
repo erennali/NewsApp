18c3ce0e5ce02d2c8aea53d7018672a190d30deb
Eren Ali Koca <eren_ali_koca@hotmail.com>
2025-03-07 21:14:13 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Eren Ali Koca on 7.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
