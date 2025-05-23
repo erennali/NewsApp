b80aa7c753b2421e4b3c14407a5728586ff6c3fd
erennali <eren_ali_koca@hotmail.com>
2025-05-23 23:52:02 +0300

    feat: app e girmeden settingsden notif. statusu değiştirme durumu eklendi

--- NewsApp/App/SceneDelegate.swift
+++ NewsApp/App/SceneDelegate.swift
@@ -51,6 +51,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        
+        // Uygulama aktif hale geldiğinde bildirim durumunu güncelleme
+        Task {
+            await NotificationManager.shared.updateNotificationStatus()
+            
+            NotificationCenter.default.post(name: .didReturnFromSettings, object: nil)
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
@@ -73,4 +80,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-
